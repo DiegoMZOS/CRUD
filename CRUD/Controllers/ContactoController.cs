776b62c07e8a5b22d2e8e98efc29830cbec42c1a
Diego <Diego@DESKTOP-EVJQMDQ>
2023-03-01 01:16:07 -0500

    Agregar archivos de proyecto.

---
+++ CRUD/Controllers/ContactoController.cs
@@ -0,0 +1,52 @@
+﻿using CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private static string conexion = ConfigurationManager.ConnectionStrings["cadena"].ToString();
+
+        private static List<Contacto> Olista = new List<Contacto>();
+        
+        // GET: Contacto
+        public ActionResult Inicio()
+        {
+            //Se inicializa la lista como una lista nueva
+            Olista = new List<Contacto>();
+            using (SqlConnection oconexion = new SqlConnection(conexion)) {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Contacto", oconexion);
+                cmd.CommandType = CommandType.Text;
+                oconexion.Open();
+
+                using (SqlDataReader dr = cmd.ExecuteReader()) { 
+                    while (dr.Read())
+                    {
+                        Contacto nuevoContacto = new Contacto();
+                        nuevoContacto.IdContacto = Convert.ToInt32(dr["IdContacto"]);
+                        nuevoContacto.Nombres = dr["Nombres"].ToString();
+                        nuevoContacto.Apellidos = dr["Apellidos"].ToString();
+                        nuevoContacto.Telefono = dr["Telefono"].ToString();
+                        nuevoContacto.Correo = dr["Correo"].ToString();
+
+                        Olista.Add(nuevoContacto);
+                    }                
+                }                     
+            }
+
+                return View(Olista);
+        }
+
+        public ActionResult Registrar() {
+        
+            return View();  
+        }
+    }
+}
---
+++ CRUD/Models/Contacto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    public class Contacto
+    {
+        public int IdContacto { get; set; }
+        public String Nombres { get; set; }
+        public String Apellidos { get; set; }
+        public String Telefono { get; set; }
+        public String Correo { get; set; }
+
+
+    }
+}
